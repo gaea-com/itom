b698a679302e8e9800f6cedadd500416500a3ae6
孙小黑 <51508636+icode-z@users.noreply.github.com>
2019-08-15 09:37:43 +0800

    清理冗余代码

--- python-itom-task/bin/itom_task.py
+++ python-itom-task/bin/itom_task.py
@@ -12,7 +12,6 @@ try:
 except ImportError as e:
     print e
     sys.exit(127)
-# import signal
 import logging.config
 from conf import config
 logging.config.dictConfig(config.LOG_CONFIG)
@@ -38,20 +37,6 @@ def init():
         os.mkdir(file_download_dir)
 
 
-# def signal_kill(sig, frame):
-#     """  捕捉到程序退出信号，进行退出操作及临时文件清理工作。  """
-#
-#     # 停止监听及任务处理进程
-#     try:
-#         for _proc in process_pool:
-#             _proc.terminate()
-#         logger.info(u'任务进程关闭成功。')
-#         system_quit.put(True)
-#
-#     finally:
-#         logger.info(u'关闭成功'.format(sig))
-
-
 def check_unfinished_task():
     # 检查 redis 中是否有未处理任务
 
@@ -70,26 +55,6 @@ def check_unfinished_task():
     logger.info(u"======>  未处理任务检查  => 完成。")
 
 
-# def create_work_process():
-#     # 创建工作进程
-#     logger.info(u"系统总核数 => {cpu},正在启动  {work_num} 个工作进程。".format(
-#         cpu=mp.cpu_count(),
-#         work_num=concurrency
-#     ))
-#     for num in range(concurrency):
-#         process_name = u"work_process_{}".format(num + 1)
-#         _proc = mp.Process(
-#             target=Workworkwork,
-#             args=(redis_cli, process_name,),
-#             name=process_name
-#         )
-#         _proc.start()
-#         logger.info(u"启动 {process_name} 进程  ===>  OK.".format(
-#             process_name=process_name
-#         ))
-#         process_pool.append(_proc)
-
-
 if __name__ == '__main__':
     # system_quit = mp.Queue()
     if not config.MAX_LISTEN_PROCESS:
@@ -103,19 +68,8 @@ if __name__ == '__main__':
     # redis 客户端初始化
     redis_cli = RedisClient.RedisConnection()
 
-    # 任务创建器初始化
-    # process_pool = []
-    # create_work_process()
-
-    # 信号初始化
-    # signal.signal(2, signal_kill)
-    # signal.signal(15, signal_kill)
-
     # 未处理任务检查
     if config.UseRedis:
         check_unfinished_task()
 
     Workworkwork(redis_cli, "main")
-
-    # 等待服务退出
-    # system_quit.get()
